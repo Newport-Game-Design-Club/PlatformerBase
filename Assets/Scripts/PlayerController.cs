9e98172bca64995aa88006576e5da3eb0a8fc8a2
Hunter Zhuang <67253838+101zh@users.noreply.github.com>
2025-11-14 20:40:47 -0800

    upload project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] float maxHorizontalSpeed = 7.5f;
+    [SerializeField] float jumpVelocity = 7.5f;
+    [SerializeField] float jumpHeightMultiplier = 0.5f;
+    [SerializeField] LayerMask jumpableSurface;
+
+    [Header("Debug Info")]
+    [SerializeField] bool isGrounded;
+    [SerializeField] float inputX;
+    [SerializeField] bool jumpKeyPressed;
+    [SerializeField] bool jumpKeyReleased;
+
+    Rigidbody2D rb2D;
+    Collider2D coll2D;
+    SpriteRenderer spriteRenderer;
+
+    // Awake is run the moment the script is run.
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        coll2D = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update() is called every frame.
+    private void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal"); // Gets the input on the horizontal axis set project settings
+
+        isGrounded = IsGrounded();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyPressed = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpKeyReleased = true;
+        }
+
+        updateAnimation();
+    }
+
+    // By default, FixedUpdate() is run every 20ms.
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb2D.velocity;
+
+        velocity.x = inputX * maxHorizontalSpeed;
+
+        if (jumpKeyPressed && isGrounded)
+        {
+            velocity.y = jumpVelocity;
+        }
+        if (jumpKeyReleased && velocity.y > 0.0f)
+        {
+            velocity.y = rb2D.velocity.y * jumpHeightMultiplier;
+        }
+
+        jumpKeyPressed = false;
+        jumpKeyReleased = false;
+
+        Debug.Log(velocity);
+        rb2D.velocity = velocity; // Sets the velocity, moving the player.
+    }
+
+    // Checks if the character is standing on the ground.
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll2D.bounds.center, coll2D.bounds.size, 0f, Vector2.down, 0.1f, jumpableSurface);
+    }
+
+    private void updateAnimation()
+    {
+        spriteRenderer.flipX = rb2D.velocity.x < 0.0f;
+    }
+}
